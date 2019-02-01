036ca5d62afb4d6fb8c7678d6673ad537550fc3f
Maciej Gołaszewski <jodator@jodator.net>
2019-02-01 15:24:05 +0100

    Create build with text transformations POC.

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -30,6 +30,110 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+
+class TextTransform extends Plugin {
+	constructor( editor ) {
+		super( editor );
+
+		editor.config.define( 'textTransform', {
+			triggers: [ ' ', '.', ',' ],
+			transformations: {
+				'CKEditor': [ 'CKEditor', 'Ckeditor', 'cKEditor', 'CK Editor' ]
+			}
+		} );
+	}
+
+	init() {
+		const editor = this.editor;
+
+		const configuredTransformations = this._getTransformationsFromConfig();
+
+		// Array with transformations to check for.
+		const transformations = Array.from( configuredTransformations.keys() );
+
+		// Characters that triggers transformations.
+		const triggers = editor.config.get( 'textTransform.triggers' );
+
+		editor.model.document.on( 'change', ( evt, batch ) => {
+			// Skip transparent batches - might came from collaboration.
+			if ( batch.type == 'transparent' ) {
+				return;
+			}
+
+			const selection = editor.model.document.selection;
+
+			// Do nothing if selection is not collapsed.
+			if ( !selection.isCollapsed ) {
+				return;
+			}
+
+			const changes = Array.from( editor.model.document.differ.getChanges() );
+			const entry = changes[ 0 ];
+
+			// Typing is represented by only a single change.
+			if ( changes.length != 1 || entry.type !== 'insert' || entry.name != '$text' || entry.length != 1 ) {
+				return;
+			}
+
+			const block = selection.focus.parent;
+			const blockText = getText( block );
+
+			const lastCharacter = blockText.slice( selection.focus.offset - 1, selection.focus.offset );
+
+			// Only check text after trigger characters.
+			if ( !triggers.includes( lastCharacter ) ) {
+				return;
+			}
+
+			// The change is always associated with one trigger character.
+			const textToTransformEnd = selection.focus.offset - 1;
+			const textBeforeInput = blockText.slice( 0, textToTransformEnd );
+
+			// Find if there is anything to transform.
+			const textToTransform = transformations.find( key => textBeforeInput.endsWith( key ) );
+
+			// Nothing found - end.
+			if ( !textToTransform ) {
+				return;
+			}
+
+			// Enqueue change to create an Undo step: this way user can always revert change with CTRL+Z.
+			editor.model.enqueueChange( writer => {
+				const transformTo = configuredTransformations.get( textToTransform );
+
+				const start = writer.createPositionAt( block, textToTransformEnd - textToTransform.length );
+				const end = writer.createPositionAt( block, textToTransformEnd );
+
+				const range = writer.createRange( start, end );
+
+				editor.model.insertContent( writer.createText( transformTo ), range );
+			} );
+		} );
+	}
+
+	_getTransformationsFromConfig() {
+		const config = this.editor.config.get( 'textTransform.transformations' );
+		const configuredTransformations = new Map();
+
+		Object.keys( config ).forEach( key => {
+			const alternations = config[ key ];
+
+			if ( Array.isArray( alternations ) ) {
+				alternations.forEach( alt => configuredTransformations.set( alt, key ) );
+			} else {
+				configuredTransformations.set( alternations, key );
+			}
+		} );
+
+		return configuredTransformations;
+	}
+}
+
+function getText( element ) {
+	return Array.from( element.getChildren() ).reduce( ( a, b ) => a + b.data, '' );
+}
+
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
@@ -52,7 +156,8 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	TextTransform
 ];
 
 // Editor configuration.
