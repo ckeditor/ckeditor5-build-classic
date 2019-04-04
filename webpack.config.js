b94038003bb8c7f3d686d29d2d26e16a6b99f06e
Maciej Bukowski <ma2ciek@gmail.com>
2019-04-04 10:31:51 +0200

    Modified webpack config for ES5 build.

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,16 @@ const path = require( 'path' );
 const webpack = require( 'webpack' );
 const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const UglifyJsWebpackPlugin = require( 'uglifyjs-webpack-plugin' );
+// const UglifyJsWebpackPlugin = require( 'uglifyjs-webpack-plugin' );
 
 module.exports = {
 	devtool: 'source-map',
 	performance: { hints: false },
 
-	entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
+	entry: [
+		require.resolve( 'regenerator-runtime/runtime.js' ),
+		path.resolve( __dirname, 'src', 'ckeditor.js' ),
+	],
 
 	output: {
 		// The name under which the editor will be exported.
@@ -31,15 +34,15 @@ module.exports = {
 
 	optimization: {
 		minimizer: [
-			new UglifyJsWebpackPlugin( {
-				sourceMap: true,
-				uglifyOptions: {
-					output: {
-						// Preserve CKEditor 5 license comments.
-						comments: /^!/
-					}
-				}
-			} )
+		// 	new UglifyJsWebpackPlugin( {
+		// 		sourceMap: true,
+		// 		uglifyOptions: {
+		// 			output: {
+		// 				// Preserve CKEditor 5 license comments.
+		// 				comments: /^!/
+		// 			}
+		// 		}
+		// 	} )
 		]
 	},
 
@@ -61,6 +64,17 @@ module.exports = {
 			{
 				test: /\.svg$/,
 				use: [ 'raw-loader' ]
+			},
+			{
+				test: /ckeditor5-[^/\\]+[/\\].*\.js$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [ require( '@babel/preset-env' ) ]
+						}
+					}
+				]
 			},
 			{
 				test: /\.css$/,
