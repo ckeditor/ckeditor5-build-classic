e52c578edca81bb9b63815bc50755f4f0653617e
Federico Moretti <hello@federicomoretti.it>
2019-10-06 14:15:44 +0200

    update ckeditor.js
    fix to issue #88 in a more React-ly way

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -28,7 +28,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
-export default class ClassicEditor extends ClassicEditorBase {}
+class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
@@ -92,3 +92,5 @@ ClassicEditor.defaultConfig = {
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
+
+export default ClassicEditor;
