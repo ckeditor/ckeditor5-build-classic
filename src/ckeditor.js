cfdd34829dc1faa449253aa6552b337f476a5888
Ilma Arifiany <ilma@fabelio.com>
2020-02-05 02:47:15 +0700

    add plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -11,6 +11,10 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -19,7 +23,6 @@ import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
@@ -28,6 +31,13 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import ImageViaUrlEmbed from '@ckeditor/image-via-url/src/imageviaurlembed';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -38,6 +48,10 @@ ClassicEditor.builtinPlugins = [
 	Autoformat,
 	Bold,
 	Italic,
+	Strikethrough,
+	Code,
+	Subscript,
+	Superscript,
 	BlockQuote,
 	CKFinder,
 	EasyImage,
@@ -46,7 +60,6 @@ ClassicEditor.builtinPlugins = [
 	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
-	ImageUpload,
 	Indent,
 	Link,
 	List,
@@ -54,30 +67,47 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	TextTransformation,
+	CodeBlock,
+	Highlight,
+	HorizontalLine,
+	RemoveFormat,
+	Alignment,
+	ImageViaUrlEmbed
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
+			'removeFormat',
 			'heading',
 			'|',
 			'bold',
 			'italic',
 			'link',
+			'alignment',
+			'|',
 			'bulletedList',
 			'numberedList',
-			'|',
 			'indent',
 			'outdent',
-			'|',
-			'imageUpload',
+			'HorizontalLine',
+			'/',
 			'blockQuote',
+			'|',
 			'insertTable',
+			'imageViaUrlEmbed',
 			'mediaEmbed',
 			'undo',
-			'redo'
+			'redo',
+			'|',
+			'subscript',
+			'superscript',
+			'highlight',
+			'code',
+			'codeBlock',
 		]
 	},
 	image: {
@@ -93,6 +123,75 @@ ClassicEditor.defaultConfig = {
 			'tableColumn',
 			'tableRow',
 			'mergeTableCells'
+		]
+	},
+	codeBlock: {
+		languages: [ {
+			language: 'plaintext',
+			label: 'Plain text'
+		}, // The default language.
+		{
+			language: 'c',
+			label: 'C',
+			class: 'language-c'
+		},
+		{
+			language: 'css',
+			label: 'CSS',
+			class: 'language-css'
+		},
+		{
+			language: 'xml',
+			label: 'HTML/XML',
+			class: 'language-xml'
+		},
+		{
+			language: 'javascript',
+			label: 'JavaScript',
+			class: 'language-javascript'
+		},
+		{
+			language: 'php',
+			label: 'PHP',
+			class: 'language-php'
+		},
+		{
+			language: 'python',
+			label: 'Python',
+			class: 'language-python'
+		}
+		]
+	},
+	highlight: {
+		options: [ {
+			model: 'yellowMarker',
+			class: 'marker-yellow',
+			title: 'Yellow Marker',
+			color: '#fed541',
+			type: 'marker'
+		},
+		{
+			model: 'greenMarker',
+			class: 'marker-green',
+			title: 'Green marker',
+			color: '#51e045',
+			type: 'marker'
+		},
+		{
+			model: 'pinkMarker',
+			class: 'marker-pink',
+			title: 'Pink marker',
+			color: '#FF007A',
+			type: 'marker'
+		},
+		{
+			model: 'blueMarker',
+			class: 'marker-blue',
+			title: 'Blue marker',
+			color: '#1FD7FF',
+			type: 'marker'
+		},
+
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
