c02990b552d892c38c9c46a723347832c982aed8
ssusko <54956745+ssusko@users.noreply.github.com>
2020-04-20 17:46:45 -0400

    Aded font

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -30,6 +30,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import Font from '@ckeditor/ckeditor5-font/src/font';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -58,7 +59,8 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+  Font
 ];
 
 // Editor configuration.
@@ -80,6 +82,10 @@ ClassicEditor.defaultConfig = {
 			'blockQuote',
 			'insertTable',
 			'mediaEmbed',
+      '|',
+      'fontSize',
+      'fontColor',
+      'fontBackgroundColor',
 			'undo',
 			'redo'
 		]
